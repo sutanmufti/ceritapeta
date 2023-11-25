cc462f27c2db30609d785514e51b035dcc6fcefa
sutan mufti <upsutan@gmail.com>
2023-11-25 12:29:34 +0000

    analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,16 @@
 
 
     <link rel="icon" href="{base}/logo.png" type="image/x-icon">
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BSNVENJNC4"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-BSNVENJNC4');
+    </script>
 </svelte:head>
 
 <main class='md:flex md:flex-col md:h-screen'>
